9bf16e8dae07becbe9e126555e140bb5ecbbe4cf
Varun Trivedi <trivedivarun2004@gmail.com>
2026-02-08 16:28:27 -0800

    Added secret Key JWT token once user logged in

--- auth-service/src/main/java/com/pm/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/com/pm/auth_service/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.pm.auth_service.controller;
 
 import com.pm.auth_service.dto.LoginRequestDTO;
 import com.pm.auth_service.dto.LoginResponseDTO;
+import com.pm.auth_service.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,12 @@ import java.util.Optional;
 
 @RestController
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
 
     @Operation(summary = "Generate token on user login")
     @PostMapping("/login")
--- auth-service/src/main/java/com/pm/auth_service/service/AuthService.java
+++ auth-service/src/main/java/com/pm/auth_service/service/AuthService.java
@@ -2,6 +2,8 @@ package com.pm.auth_service.service;
 
 import com.pm.auth_service.dto.LoginRequestDTO;
 import com.pm.auth_service.model.User;
+import com.pm.auth_service.util.JwtUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -9,9 +11,13 @@ import java.util.Optional;
 @Service
 public class AuthService {
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
-    public AuthService(UserService userService) {
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
     }
 
     public Optional<String> authenticate(LoginRequestDTO loginRequestDTO){
